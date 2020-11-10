a5b3c4b26c21afb1645638da122dee236d8a0756
Lucca Lopes <62670099+Luccacpl@users.noreply.github.com>
2020-11-10 18:52:36 -0300

    Pagina CadastroTrabalhador refeita

--- src/pages/CadastroTrabalhador.tsx
+++ src/pages/CadastroTrabalhador.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { FiPlus, FiArrowLeft } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import CustomButton from '../components/CustomButton';
 
@@ -40,45 +42,118 @@ const stylePink = {
 function CadastroTrabalhador(){
     const classes = useStyles();
 
+    const [images, setImages] = useState<File[]>([]);
+    const [previewImages, setPreviewImages] = useState<string[]>([]);
+
+    function handleSelectedImages(event: ChangeEvent<HTMLInputElement>) {
+        if(!event.target.files){
+            return;
+        }
+
+        const selectedImages = Array.from(event.target.files);
+
+        setImages(selectedImages);
+
+        const selectedImagesPreview = selectedImages.map(image => {
+            return URL.createObjectURL(image);
+          });
+      
+        setPreviewImages(selectedImagesPreview);
+    }
+
     return(
         <div id="page-cadastro-trabalhador">
-            <form className="wrapper">
-                <Grid container spacing={3}>
+            <aside className="app-sidebar">
+                <h1>Faxilita</h1>
+                <footer>
+                    <Link to="/">
+                        <button>
+                            <FiArrowLeft size={24} color="#FFF" />
+                        </button>
+                    </Link>
+                </footer>
+            </aside>
 
-                    <Grid item xs={12}>
-                        <h1 className={classes.Title}>Registrar novo Trabalhador</h1>
+            <main>
+                <form className="wrapper">
+                    <Grid container spacing={3}>
+
+                        <Grid item xs={12} className="fixed">
+                                <h1 className={classes.Title}>Registrar novo Trabalhador</h1>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <hr />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <label htmlFor="name">Nome</label>
+                            <input  placeholder="Ex: José" />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <label htmlFor="name">Sobrenome</label> 
+                            <input  placeholder="Ex: Silva" />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <label htmlFor="name">Email</label>
+                            <input type="email" placeholder="Ex: teste@email.com" />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <label htmlFor="name">Confirmação de Email</label>
+                            <input type="email" placeholder="Ex: teste@email.com" />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <label htmlFor="name">Senha</label>
+                            <input type="password" placeholder="Insira sua senha" />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <label htmlFor="name">Confirmação de Senha</label>
+                            <input type="password" placeholder="Confirme sua senha" />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <label htmlFor="name">Número de celular</label>
+                            <input  placeholder="Ex: (13)99999-9999" />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <label htmlFor="name">CPF</label>
+                            <input  placeholder="Ex: 000-000-000-00" />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <label htmlFor="name">Data de Nascimento</label>
+                            <input type="date" placeholder="Data de nascimento" />
+                        </Grid>
+
+                        <Grid item xs={12} className="fixed">
+                                <h1 className={classes.Title}>Foto</h1>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <hr />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <label className="custom-file-upload">
+                                <FiPlus size={24} color="black"/>
+                                <input onChange={handleSelectedImages} type="file">
+                                </input>
+                            </label>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <CustomButton text="Registrar" style={Object.assign({}, Botao, stylePink)}/>
+                        </Grid>
+
                     </Grid>
-
-                    <Grid item xs={6}>
-                        <input className="txtNome" placeholder="Nome" />
-                    </Grid>
-
-                    <Grid item xs={6}> 
-                        <input className="txtSobrenome" placeholder="Sobrenome" />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <input type="email" placeholder="Email" />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <input type="password" placeholder="Senha" />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <input type="password" placeholder="Confirmação de senha" />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <input  placeholder="CPF" />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <CustomButton text="Registrar" style={Object.assign({}, Botao, stylePink)}/>
-                    </Grid>
-
-                </Grid>
-            </form>
+                </form>
+            </main>
         </div>
     );
 }
