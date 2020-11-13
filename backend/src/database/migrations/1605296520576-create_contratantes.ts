442ae4737f8edc4d15bb10a8c9b80ebf543739f7
Lucca Lopes <62670099+Luccacpl@users.noreply.github.com>
2020-11-13 19:11:26 -0300

    Criando primeira tabela do Banco de Dados.

--- backend/src/database/migrations/1605296520576-create_contratantes.ts
+++ backend/src/database/migrations/1605296520576-create_contratantes.ts
@@ -1,11 +1,45 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class createContratantes1605296520576 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+      // Realiza alterações no BD
+      // Criar Tabela, criar novo campo, deletar algum campo
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(new Table({
+        name: 'contratantes',
+        columns : [
+          {
+            name: 'cd_cpf_contratante',
+            type: 'integer',
+            unsigned: true,
+            isPrimary: true,
+            isGenerated: false,
+          },
+          {
+            name: 'nm_contratante',
+            type: 'varchar'
+          },
+          {
+            name: 'dt_nascimento_contratante',
+            type: 'date'
+          },
+          {
+            name: 'ds_contratante',
+            type: 'text'
+          },
+          {
+            name: 'ic_sexo_contratante',
+            type: 'boolean'
+          }
+        ],
+      }))
+  }
+
+        // Desfazer oq foi feito no UP
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('contratantes');
+  }
 
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Contratante from './models/Contratante';
 
 import './database/connection';
 
@@ -6,8 +8,28 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/users', (request, response) => {
-    return response.json({ message: 'Hello world' });
+app.post('/contratantes', async (request, response) => {
+    const {
+        cd_cpf_contratante,
+        nm_contratante,
+        dt_nascimento_contratante,
+        ds_contratante,
+        ic_sexo_contratante,
+    } = request.body;
+
+    const contratanteRepository = getRepository(Contratante);
+
+    const contratante = contratanteRepository.create({
+        cd_cpf_contratante,
+        nm_contratante,
+        dt_nascimento_contratante,
+        ds_contratante,
+        ic_sexo_contratante,
+    });
+
+    await contratanteRepository.save(contratante);
+
+    return response.status(201).json(contratante);
 });
 
 app.listen(3333);                 //localhost:3333
