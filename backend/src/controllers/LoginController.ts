7b6b24dd94c38e2557f10458b8330516726b4c43
Lucca Lopes <62670099+Luccacpl@users.noreply.github.com>
2020-11-22 20:11:08 -0300

    Adicionando comentarios

--- backend/src/controllers/LoginController.ts
+++ backend/src/controllers/LoginController.ts
@@ -7,17 +7,17 @@ import Usuario from '../models/Usuario';
 
 export default {
     async login(request: Request, response: Response) {
-        const {cd_cpf_usuario, senha} = request.body; 
+        const {cd_cpf_usuario, senha} = request.body;                                                           // Pegando campos do banco de dados que serão utilizados para o login
 
         const usuarioRepository = getRepository(Usuario);
 
         try{
-            const usuario = await usuarioRepository.findOneOrFail(cd_cpf_usuario);
+            const usuario = await usuarioRepository.findOneOrFail(cd_cpf_usuario);                              // Verificando se o cpf existe
             console.log(usuario);
-            if(Bcrypt.compareSync(senha, usuario.senha)){
-                return response.status(200).json();
+            if(Bcrypt.compareSync(senha, usuario.senha)){                                                       // Verificando se a senha digitada existe
+                return response.status(200).json();                                                             // Caso exista, ele retorna o codigo 200
             }else{
-                return response.status(401).json({ user: "not found"})
+                return response.status(401).json({ user: "not found"})                                          // Caso não exista ele retorna o codigo 401
             }
         }
         catch(e) {
